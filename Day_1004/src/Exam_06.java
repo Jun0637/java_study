dcf5630d7a6fb8700dbeeb81071b0db8e8cb166e
Jun0637 <105259262+Jun0637@users.noreply.github.com>
2022-10-04 12:43:27 +0900

    Add files via upload

---
+++ Day_1004/src/Exam_06.java
@@ -0,0 +1,19 @@
+// 센티미터 단위의 정수를 실수형 인치 단위로 변환하여 출력하시오.
+// 1inch는 2.54cm
+public class Exam_06 {
+
+	public static void main(String[] args) {
+		
+		int cm;
+		double inch;
+		
+		cm = 17;
+		inch = 2.54;
+		
+		System.out.println(cm * inch);
+		
+
+
+	}
+
+}
