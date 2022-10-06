890d5654e5a654d85d6be7200412fdb012a8d857
Jun0637 <105259262+Jun0637@users.noreply.github.com>
2022-10-06 09:42:09 +0900

    Add files via upload

---
+++ Day_1005/src/Exam_04.java
@@ -0,0 +1,13 @@
+// 제품의 원가와 실제구매가격을 입력하여 얼마나 더 비싸게 또는 싸게 샀는지 출력하는 프로그램을 작성 하시오.
+public class Exam_04 {
+	
+	public static void main(String[] args) {
+	
+	int a = 15;
+	boolean result;
+	
+	result = a %3 ==0 || a %5 ==0 ;
+	
+	System.out.println(result);
+	}
+}
