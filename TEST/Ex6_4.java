5ed02e5d0fa55db9904da7558cba43a19d45ffa5
Jun0637 <105259262+Jun0637@users.noreply.github.com>
2022-12-22 21:30:18 +0900

    Add files via upload

--- TEST/Ex6_4.java
+++ TEST/Ex6_4.java
@@ -7,7 +7,8 @@ class Ex6_4 {
 		long result2 = mm.subtract(5L, 3L);
 		long result3 = mm.multiply(5L, 3L);
 		double result4 = mm.divide(5L, 3L);
-
+		mm.printGuguda(15);
+		
 		System.out.println("add(5L, 3L) = " + result1);
 		System.out.println("subtract(5L, 3L) = " + result2);
 		System.out.println("multiply(5L, 3L) = " + result3);
@@ -17,10 +18,24 @@ class Ex6_4 {
 }
 
 class MyMath{
+		void printGuguda(int dan) {
+			if(!(2<dan &&dan <=9))
+				return;
+			for(int i=1;i<=9;i++) {
+				System.out.printf("%d* %d = %d%n",dan, i, dan * i);
+			}
+		}
 	long add(long a, long b) {
 		long result = a+b;
 		return result;
 		// return a + b;  위 둑줄을 이와 같이 한줄로 간단히 할수있다.
+	}
+	//두 값을 받아서 둘중에 큰 값을 반환하는 메서드를 작성하시오.
+	long max(long a, long b) {
+		if(a>b)
+			return a;// 조건식이 참일때만 실행
+		else
+			return b; // 조건식이 거짓일때 실행
 	}
 	long subtract(long a, long b) {return a - b;}
 	long multiply(long a, long b) {return a * b;}
